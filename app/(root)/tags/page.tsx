bf1e4d6eb491df087ccdadd66d78df74e58f5595
ChrismerinoDev <merinochrisdm@gmail.com>
2025-03-26 12:40:05 -0400

    Display Tags

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -1,19 +1,54 @@
 import React from "react";
 
+import TagCard from "@/components/cards/TagCard";
+import DataRenderer from "@/components/DataRenderer";
+import LocalSearch from "@/components/search/LocalSearch";
+import ROUTES from "@/constants/routes";
 import { getTags } from "@/lib/actions/tag.action";
 
-const Tags = async () => {
+import { EMPTY_TAGS } from "../../../constants/states";
+
+const Tags = async ({ searchParams }: RouteParams) => {
+  const { page, pageSize, query, filter } = await searchParams; // De-structuring the data being passed as the searchParams.
+
   const { success, data, error } = await getTags({
-    page: 1,
-    pageSize: 10,
-    query: "",
+    page: Number(page) || 1, // Converting the url value to a Number, because the value by default is a "string".
+    pageSize: Number(pageSize) || 10,
+    query,
+    filter,
   });
 
   const { tags } = data || {};
 
-  console.log("TAGS", JSON.stringify(tags, null, 2));
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900 text-3xl">Tags</h1>
 
-  return <div>Tags</div>;
+      <section className="mt-11">
+        <LocalSearch
+          route={ROUTES.TAGS}
+          imgSrc="/icons/search.svg"
+          placeholder="Search by tag name..."
+          // iconPosition="left"
+          otherClasses="flex-1"
+        />
+      </section>
+
+      <DataRenderer
+        success={success}
+        error={error}
+        data={tags}
+        empty={EMPTY_TAGS}
+        render={(tags) => (
+          <div className="mt-10 flex w-full flex-wrap gap-4">
+            {tags.map((tag) => (
+              <TagCard key={tag._id} {...tag} />
+            ))}
+          </div>
+        )}
+      />
+    </>
+  );
 };
 
 export default Tags;
